e957eb7d92b3235fe3198d7f48e02203fc1b7fe6
Anatoli <anatoliy@brainamics.de>
2023-06-20 16:19:30 +0200

    Fixed docker

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 
 app.post('/breachedaccount', async (req, res) => {
   const email = req.body.email;
-  const url = `https://haveibeenpwned.com/api/v2/breachedaccount/${email}`;
+  const url = `https://haveibeenpwned.com/api/v3/breachedaccount/${email}`;
 
   try {
     const response = await fetch(url, {
